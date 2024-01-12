2a75ab3cfe760021f049359a7dc151f4a0c1cae0
DavidCabrerizo <74828595+DavidCabrerizo@users.noreply.github.com>
2024-01-12 14:04:36 +0100

    Add files via upload

---
+++ src/PruebaComponente.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PruebaComponente() {
+  return (
+    <div>Prueba Componente</div>
+  )
+}
+
+export default PruebaComponente
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="https://www.cannabis24h.com">Cannabis24h</a>
+    </div>
+    <div>
+      <a href="https://instagram.com/david_madrid_19200">Siguenos en Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
